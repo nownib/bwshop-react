e458e3022a2c6ca9c7ccc5dc7fb7105cae6711e7
nownib <binnlw03@gmail.com>
2024-10-07 23:40:47 +0700

    fix bug login google

--- src/components/Login/GoogleRedirect.js
+++ src/components/Login/GoogleRedirect.js
@@ -1,21 +1,19 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { loginRedux } from "../../redux/action/actions";
 
 const GoogleRedirect = () => {
   const navigate = useNavigate();
   const handleLoginWithGoogle = async (token) => {
     if (token) {
       localStorage.setItem("Bearer", token);
-      window.location.reload();
-      navigate("/");
+      window.location.href("/");
     }
   };
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const token = urlParams.get("token");
     handleLoginWithGoogle(token);
-  }, []);
+  }, [navigate]);
 
   return <></>;
 };
